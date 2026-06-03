907f8fd9659cb4b5970af24c92824121a9de91e9
Alfie-Wang3 <alfiework3@gmail.com>
2026-06-03 23:04:18 +0800

    member.sql

--- sql/members.sql
+++ sql/members.sql
@@ -2,14 +2,16 @@ CREATE DATABASE IF NOT EXISTS members;
 USE members;
 
 DROP TABLE IF EXISTS members;
-# members資料表
+
 CREATE TABLE members(
-	id VARCHAR(25) PRIMARY KEY NOT NULL,
-    passwords VARCHAR(30) NOT NULL
+    id VARCHAR(25) PRIMARY KEY NOT NULL,
+    passwords VARCHAR(30) NOT NULL,
+    role ENUM('customer','admin') NOT NULL DEFAULT 'customer'
 );
 
-INSERT INTO members(id,passwords) VALUES
-('root','1234'),
-('02',5678);
+-- 管理員帳號 (root) 顧客帳號 (02)
+INSERT INTO members(id,passwords,role) VALUES
+('root','1234','admin'),
+('02','5678','customer');
 
 SELECT * FROM members;
