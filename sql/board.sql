29f132ca867908a91896db0378eac22af6c4fa22
Sylvie <minnie.yen311@gmail.com>
2026-06-09 02:09:51 +0800

    新增orders資料表

--- sql/board.sql
+++ sql/board.sql
@@ -1,11 +1,20 @@
 CREATE DATABASE IF NOT EXISTS board;
 USE board;
 
-
+DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS form;
 DROP TABLE IF EXISTS guestbook;
 
-#form資料表
+#orders 資料表
+CREATE TABLE orders (
+    order_id VARCHAR(20) PRIMARY KEY COMMENT '訂單編號',
+    member_id VARCHAR(50) NOT NULL COMMENT '會員帳號(對應登入ID)',
+    order_date DATE NOT NULL COMMENT '訂購日期',
+    total_amount INT NOT NULL COMMENT '總金額',
+    status VARCHAR(20) NOT NULL COMMENT '訂單狀態'
+);
+
+#form 資料表
 CREATE TABLE form(
     new_name VARCHAR(10) PRIMARY KEY,
     new_mail VARCHAR(25),
@@ -13,16 +22,22 @@ CREATE TABLE form(
     new_content VARCHAR(100)
 );
 
-#guestbook資料表
+#guestbook 資料表
 CREATE TABLE IF NOT EXISTS guestbook (
   GBNO INT NOT NULL AUTO_INCREMENT COMMENT '留言編號' PRIMARY KEY,
-  GBName VARCHAR(20) NOT NULL COMMENT '訪客姓名',
-  Mail VARCHAR(50) DEFAULT NULL COMMENT 'E-mail',
-  Subject VARCHAR(50) NOT NULL COMMENT '留言主題',
+  GBName VARCHAR(50) NOT NULL COMMENT '訪客姓名',
+  Mail VARCHAR(100) DEFAULT NULL COMMENT 'E-mail',
+  Subject VARCHAR(255) NOT NULL COMMENT '留言主題',
   Content TEXT DEFAULT NULL COMMENT '留言內容',
   Putdate DATE NOT NULL COMMENT '留言時間'
 ); 
 
+#訂單資料
+INSERT INTO orders (order_id, member_id, order_date, total_amount, status) VALUES
+('KB1768', '02', '2025-11-20', 1500, '待出貨'),
+('KL3978', '02', '2025-06-10', 7430, '已出貨'),
+('KA7217', '02', '2025-12-10', 8733, '待出貨'),
+('BC1038', 'user03', '2025-12-12', 10345, '待出貨');
 
 INSERT INTO guestbook (GBName, Mail, Subject, Content, Putdate) VALUES
 ('李曉明', 'ming@gmail.com', 'N95醫療口罩', '出貨速度很快，服務態度良好', '2026-05-27'),
@@ -30,5 +45,6 @@ INSERT INTO guestbook (GBName, Mail, Subject, Content, Putdate) VALUES
 ('王寶釧', 'bouchun@gmail.com', '防水透氣ok蹦', '品質好的沒話說，賣家回復快速', '2026-05-27'),
 ('李樹隨', 'tree@gmail.com', '親水性敷料人工皮', '這是一次非常滿意的購物經驗', '2026-05-27');
 
-
+SELECT * FROM orders;
+SELECT * FROM form;
 SELECT * FROM guestbook;
